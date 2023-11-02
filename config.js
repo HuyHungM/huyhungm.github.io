73bcbce15e9fd9c64b800ee00b1eec6e04e7e8bd
HuyHungM <nguyenhuyhung161111@gmail.com>
2023-11-02 23:10:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,4 @@ module.exports = {
   port: "0210" || process.env.Port,
   cookieSecret: "s%3Al3ozSdvQ83TtC5RvJ.CibaQoHtaY0H3QOB1kqR8H2A" || process.env.CookieSecret,
   allowedWebsites: ["https://truyenhub.nguyenhuy144.repl.co"],
-  uploadSizeLimit: 100 // MB unit
 }
