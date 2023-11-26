d70a518f44e7e3a69e477f4a235794b5c1c39c83
HuyHungM <nguyenhuyhung161111@gmail.com>
2023-11-26 21:38:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: "0210" || process.env.Port,
+  port: "" || process.env.Port,
   cookieSecret: "s%3Al3ozSdvQ83TtC5RvJ.CibaQoHtaY0H3QOB1kqR8H2A" || process.env.CookieSecret,
   allowedWebsites: ["https://animehub.hungnguyen194.repl.co"],
 }
